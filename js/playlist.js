4ca5ecbf0a9e04f47a31ba8b2a3c6758e3a092ef
Alexander Krastev <103534450+Alexander6560@users.noreply.github.com>
2023-02-10 13:34:48 +0200

    New
    New

---
+++ js/playlist.js
@@ -0,0 +1,64 @@
+let songs = [
+  {
+    name: "CHASE",
+    artist: "AGK Beatz",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "FOREIGN",
+    artist: "AGK Beatz",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "FREEZER",
+    artist: "AGK Beatz",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "NO LIMIT",
+    artist: "AGK Beatz",
+    img: "img4",
+    audio: "music4"
+  },
+  {
+    name: "NOSTALGIA",
+    artist: "AGK Beatz",
+    img: "img5",
+    audio: "music5"
+  },
+  {
+    name: "NOTORIOUS",
+    artist: "AGK Beatz",
+    img: "img6",
+    audio: "music6"
+  },
+  {
+    name: "RICH",
+    artist: "AGK Beatz",
+    img: "img7",
+    audio: "music7"
+  }
+  ,
+  {
+    name: "SNAKE",
+    artist: "AGK Beatz",
+    img: "img8",
+    audio: "music8"
+  },
+  {
+    name: "TOXIC",
+    artist: "AGK Beatz",
+    img: "img9",
+    audio: "music9"
+  },
+  {
+    name: "WARNING",
+    artist: "AGK Beatz",
+    img: "img10",
+    audio: "music10"
+  }
+
+]
